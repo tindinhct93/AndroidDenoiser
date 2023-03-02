507e0c06aa517e326697fa45fe6b58f7f9b0ec2e
tindinhct93 <tindinhct93@gmail.com>
2023-03-02 10:11:55 +0700

    change

--- README.md
+++ README.md
@@ -1,17 +1,39 @@
-# Using scikit-learn Models In Android Applications
+# Android Audio Speech Enhancement App
 
-This project demonstrates the use of a scikit-learn model in an Android app using ONNX as a bridge between both the frameworks.
+## Overview
+This project contains an Android app that demonstrates how to use the ONNX Runtime to perform audio recognition using a pre-trained model. The app records a 1 second audio clip, reads the clip into a tensor, and passes it through the pre-trained model to obtain a prediction. The app then displays the prediction, as well as the inference time.
 
-![Demo of the app](images/sklearn_model_demo_android.gif)
 
-## Getting Started
+## Using the App
 
-1. Clone this repository and open the resulting project in Android Studio,
 
-```
->> git clone https://github.com/shubham0204/Scikit_Learn_Android_Demo
-```
+When you run the app, you will see a simple interface with a record button and a text view. To use the app, follow these steps:
 
-2. Read the blog [Deploying Scikit-Learn Models In Android Apps With ONNX](https://towardsdatascience.com/deploying-scikit-learn-models-in-android-apps-with-onnx-b3adabe16bab) to follow the procedure for your model. The code included in this blog is available as an [Google Colab notebook](https://colab.research.google.com/github/shubham0204/Google_Colab_Notebooks/blob/main/Sklearn_to_ONNX_Conversion.ipynb).
+1. Press the record button to start recording.
 
-3. Place your `.ort` model in the `app/src/main/res/raw` folder.
+2. Speak into the microphone for 1 second.
+
+3. The app will automatically stop recording after 1 second.
+
+4. Press the Predict button to perform audio recognition on the recorded clip.
+
+5. The app will display the prediction and the inference time in the text view.
+
+## Understanding the Code
+
+The app consists of a single activity, MainActivity, which contains the code for recording audio, reading the audio clip into a tensor, and performing inference using the pre-trained model.
+
+
+The code is divided into several functions:
+
+- onCreate: This function is called when the activity is created. It initializes the views and creates an instance of the ONNX Runtime environment and session.
+
+- readWavFileToTensor: This function takes a WAV file as input and returns a 1 second tensor of floating point values. The function reads the WAV file header to obtain the sample rate and number of channels, and then reads the audio data into a byte array. The function converts the byte array into a float array by dividing each 16-bit sample by Short.MAX_VALUE. The function returns the first 16000 samples of the float array (corresponding to the first second of audio).
+
+- readTXT: This function reads a text file containing a random tensor and returns the tensor as a float array.
+
+- createORTSession: This function creates an instance of the ONNX Runtime session using the pre-trained model.
+
+- runFilePrediction: This function takes a tensor of audio data as input and returns the output of the pre-trained model as a float array. The function creates an input tensor from the audio data and passes it to the ONNX Runtime session. The function also measures the inference time and includes it in the output float array.
+
+- runPrediction: This function is similar to runFilePrediction, but instead of taking a tensor of audio data as input, it generates a random tensor and passes it to the ONNX Runtime session.
--- app/src/main/java/com/mobileml/shubham0204/scikitlearndemo/MainActivity.kt
+++ app/src/main/java/com/mobileml/shubham0204/scikitlearndemo/MainActivity.kt
@@ -3,13 +3,19 @@ package com.mobileml.shubham0204.scikitlearndemo
 import ai.onnxruntime.OnnxTensor
 import ai.onnxruntime.OrtEnvironment
 import ai.onnxruntime.OrtSession
-import androidx.appcompat.app.AppCompatActivity
+import android.R.attr.shape
+import android.content.Context
 import android.os.Bundle
 import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
-import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 import java.nio.FloatBuffer
+import java.util.*
+import java.io.*
+import java.nio.ByteBuffer
+import java.nio.ByteOrder
+
 
 class MainActivity : AppCompatActivity() {
 
@@ -18,46 +24,161 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         // Initialize the views
-        val inputEditText = findViewById<EditText>( R.id.input_edittext )
+        val inputEditTextView = findViewById<TextView>( R.id.textView )
         val outputTextView = findViewById<TextView>( R.id.output_textview )
-        val button = findViewById<Button>( R.id.predict_button )
+        val secondTextView = findViewById<TextView>( R.id.time_textview )
+        val predict_button = findViewById<Button>( R.id.predict_button )
 
-        button.setOnClickListener {
+        // Initialize the ORT environment and create the ORT session.
+        val ortEnvironment = OrtEnvironment.getEnvironment()
+        val ortSession = createORTSession( ortEnvironment )
+
+        val context: Context = this // Obtain the current context from the activity
+        val wavFile = File(context.resources.openRawResource(R.raw.debug).use { inputStream ->
+            val file = File.createTempFile("temp", ".wav")
+            file.outputStream().use { outputStream ->
+                inputStream.copyTo(outputStream)
+            }
+            file.absolutePath
+        })
+
+        predict_button.setOnClickListener {
             // Parse input from inputEditText
-            val inputs = inputEditText.text.toString().toFloatOrNull()
-            if ( inputs != null ) {
-                val ortEnvironment = OrtEnvironment.getEnvironment()
-                val ortSession = createORTSession( ortEnvironment )
-                val output = runPrediction( inputs , ortSession , ortEnvironment )
-                outputTextView.text = "Output is ${output}"
+            val FloatTensor = readWavFileToTensor(wavFile)
+
+            val output = runFilePrediction( ortSession , ortEnvironment ,FloatTensor)
+            val i= 12000
+            outputTextView.text = "Inference Time is ${output[0]}us. Output is ${output[i+1]},${output[i+2]},${output[i+3]},${output[i+4]},${output[i+5]},${output[i+6]},${output[i+7]},${output[i+8]}. "
+            secondTextView.text = output.size.toString()
+            inputEditTextView.text = FloatTensor.size.toString()
+        }
+    }
+
+    /**
+     * This method reads a WAV file and converts it into a 1-second tensor of floats.
+     * @param wavFile - the input WAV file
+     * @return the tensor of floats
+     */
+    fun readWavFileToTensor(wavFile: File): FloatArray {
+        val inputStream = FileInputStream(wavFile)
+
+        val header = ByteArray(44)
+        inputStream.read(header)
+
+        val sampleRate = ByteBuffer.wrap(header, 24, 4).order(ByteOrder.LITTLE_ENDIAN).int
+        val numChannels = ByteBuffer.wrap(header, 22, 2).order(ByteOrder.LITTLE_ENDIAN).short.toInt()
+
+        val data = ByteArray(wavFile.length().toInt() - 44)
+        inputStream.read(data)
+
+        var max_length = 16000
+        if (data.size/2< max_length) max_length = data.size/2
+        val floatData = FloatArray(max_length)
+        for (i in floatData.indices) {
+            floatData[i] = ByteBuffer.wrap(data, i * 2, 2).order(ByteOrder.LITTLE_ENDIAN).short.toFloat() / Short.MAX_VALUE
+        }
+        return floatData
+    }
+
+    /**
+     * This method reads a text file and returns a float array containing the values in the file.
+     * @return the float array containing the values in the file
+     */
+    private fun readTXT() : FloatArray {
+        val inputStream = resources.openRawResource(R.raw.tensor)
+
+        val reader = BufferedReader(InputStreamReader(inputStream))
+
+        val floatList = mutableListOf<Float>()
+
+        try {
+            // Read each line of the file and convert it to a float
+            var line: String?
+            while (reader.readLine().also { line = it } != null) {
+                val value = line!!.toFloat()
+                floatList.add(value)
             }
-            else {
-                Toast.makeText( this , "Please check the inputs" , Toast.LENGTH_LONG ).show()
-            }
+        } catch (e: IOException) {
+            e.printStackTrace()
         }
 
+        val floatArray = FloatArray(floatList.size)
+        for (i in floatList.indices) {
+            floatArray[i] = floatList[i]
+        }
+        return floatArray
     }
 
     // Create an OrtSession
     private fun createORTSession( ortEnvironment: OrtEnvironment ) : OrtSession {
-        val modelBytes = resources.openRawResource( R.raw.sklearn_model ).readBytes()
+        val modelBytes = resources.openRawResource( R.raw.dynamic_model ).readBytes()
         return ortEnvironment.createSession( modelBytes )
     }
 
-    // Make predictions with given inputs
-    private fun runPrediction( input : Float , ortSession: OrtSession , ortEnvironment: OrtEnvironment ) : Float {
+    //run prediction with the input from files.
+    private fun runFilePrediction( ortSession: OrtSession , ortEnvironment: OrtEnvironment, InputFloatAraay: FloatArray ) : FloatArray {
+        // Get the name of the input node
+        val inputName = ortSession.inputNames?.iterator()?.next()
+
+        val floatBuffer = FloatBuffer.wrap(InputFloatAraay)
+
+        // Create input tensor with floatBufferInputs of shape ( 1 , 1 )
+        val inputTensor = OnnxTensor.createTensor(
+            ortEnvironment, floatBuffer, longArrayOf(
+                1, 1, InputFloatAraay.size.toLong()
+            )
+        )
+        // Run the model
+
+        val tStart = System.currentTimeMillis()
+        val results = ortSession.run(mapOf(inputName to inputTensor))
+
+        val tEnd = System.currentTimeMillis()
+        val tDelta = tEnd - tStart
+
+        val ouput = results[0].value as Array<Array<FloatArray>>
+        val float_waveform = ouput[0][0]
+        float_waveform[0] = tDelta.toFloat()
+
+        return float_waveform
+    }
+
+    // Make predictions with dummy inputs
+    private fun runPrediction( ortSession: OrtSession , ortEnvironment: OrtEnvironment, rand: Random ) : FloatArray {
         // Get the name of the input node
         val inputName = ortSession.inputNames?.iterator()?.next()
         // Make a FloatBuffer of the inputs
-        val floatBufferInputs = FloatBuffer.wrap( floatArrayOf( input ) )
+        var numbers = FloatArray(44100)
+
+        for (i in 0..44099) {
+            numbers[i] = 1.0f
+        }
+        numbers = readTXT()
+
+        val floatBuffer = FloatBuffer.wrap(numbers)
+
         // Create input tensor with floatBufferInputs of shape ( 1 , 1 )
-        val inputTensor = OnnxTensor.createTensor( ortEnvironment , floatBufferInputs , longArrayOf( 1, 1 ) )
+        val inputTensor = OnnxTensor.createTensor(
+            ortEnvironment, floatBuffer, longArrayOf(
+                1, 1, 44100
+            )
+        )
+
         // Run the model
-        val results = ortSession.run( mapOf( inputName to inputTensor ) )
+        val tStart = System.currentTimeMillis()
+        val results = ortSession.run(mapOf(inputName to inputTensor))
+
+        for (i in 1..9) {
+            ortSession.run(mapOf(inputName to inputTensor))
+        }
         // Fetch and return the results
-        val output = results[0].value as Array<FloatArray>
-        return output[0][0]
+        val tEnd = System.currentTimeMillis()
+        val tDelta = tEnd - tStart
+
+        val ouput = results[0].value as Array<Array<FloatArray>>
+        val float_waveform = ouput[0][0]
+        float_waveform[0] = tDelta.toFloat()
+
+        return float_waveform
     }
-
-
 }
